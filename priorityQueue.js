1b1387b287dd32699c1d3a57b6e96551976dd3a7
Alina <llliinaa.a08@gmail.com>
2026-04-08 17:52:58 +0300

    Додано приватний метод _findIndex для обчислення індексів за пріоритетом та часом

--- priorityQueue.js
+++ priorityQueue.js
@@ -12,4 +12,21 @@ export class BiDirectionalPriorityQueue {
       order: this.insertionCounter++ 
     });
   }
+
+  _findIndex(criteria) {
+    if (this.items.length === 0) return -1;
+
+    if (criteria === 'oldest') return 0; 
+    if (criteria === 'newest') return this.items.length - 1; 
+
+    let targetIndex = 0;
+    for (let i = 1; i < this.items.length; i++) {
+      if (criteria === 'highest' && this.items[i].priority > this.items[targetIndex].priority) {
+        targetIndex = i;
+      } else if (criteria === 'lowest' && this.items[i].priority < this.items[targetIndex].priority) {
+        targetIndex = i;
+      }
+    }
+    return targetIndex;
+  }
 }
