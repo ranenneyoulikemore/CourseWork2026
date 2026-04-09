e697cda360926dc67fc4c92f79e876d7a5f50013
RANENNE <alexdriha9280@gmail.com>
2026-04-09 03:39:25 +0300

    get elements by priority or insertion order

--- src/tools/priorityQueue.js
+++ src/tools/priorityQueue.js
@@ -13,4 +13,19 @@ class PriorityQueue {
             order: this.insertOrder++
         });
     }
+
+    peek(type){
+        if (this.items.length === 0) return null;
+
+        let targetIndx = 0;
+        for ( let i = 1; i < this.items.length; i++){
+            if (type === 'highest' && this.items[i].priority > this.items[targetIndx].priority) targetIndx = i;
+            if (type === 'lowest' && this.items[i].priority < this.items[targetIndx].priority) targetIndx = i;
+            if (type === 'oldest' && this.items[i].order > this.items[targetIndx].order) targetIndx = i;
+            if (type === 'newest' && this.items[i].order < this.items[targetIndx].order) targetIndx = i;
+        }
+        return this.items[targetIndx].item;
+    
+    }
 }
+
