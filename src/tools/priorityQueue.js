304576f907c7962853e72b388e9e7cd324747652
RANENNE <alexdriha9280@gmail.com>
2026-04-11 04:18:23 +0300

    implement dequeue method

--- src/tools/priorityQueue.js
+++ src/tools/priorityQueue.js
@@ -27,5 +27,19 @@ class PriorityQueue {
         return this.items[targetIndx].item;
     
     }
+
+    dequeue(type) {
+        if (this.items.length === 0) return null;
+
+        let targetIndx = 0;
+        for (let i = 0; i < this.items.length; i++){
+            if (type === 'highest' && this.items[i].priority > this.items[targetIndx].priority) targetIndx = i;
+            if (type === 'lowest' && this.items[i].priority < this.items[targetIndx].priority) targetIndx = i;
+            if (type === 'oldest' && this.items[i].order > this.items[targetIndx].order) targetIndx = i;
+            if (type === 'newest' && this.items[i].order < this.items[targetIndx].order) targetIndx = i;
+        }
+        const removed = splice(targetIndx, 1) [0];
+        return removed.item;
+    }
 }
 
