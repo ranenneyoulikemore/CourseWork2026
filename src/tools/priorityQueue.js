893cd4ec4c9c434dc1895f6ad443ed0b5696bfa0
RANENNE <alexdriha9280@gmail.com>
2026-04-11 04:24:53 +0300

    correct splice call, fix oldest/newest logic

--- src/tools/priorityQueue.js
+++ src/tools/priorityQueue.js
@@ -21,8 +21,8 @@ class PriorityQueue {
         for ( let i = 1; i < this.items.length; i++){
             if (type === 'highest' && this.items[i].priority > this.items[targetIndx].priority) targetIndx = i;
             if (type === 'lowest' && this.items[i].priority < this.items[targetIndx].priority) targetIndx = i;
-            if (type === 'oldest' && this.items[i].order > this.items[targetIndx].order) targetIndx = i;
-            if (type === 'newest' && this.items[i].order < this.items[targetIndx].order) targetIndx = i;
+            if (type === 'oldest' && this.items[i].order < this.items[targetIndx].order) targetIndx = i;
+            if (type === 'newest' && this.items[i].order > this.items[targetIndx].order) targetIndx = i;
         }
         return this.items[targetIndx].item;
     
@@ -32,13 +32,13 @@ class PriorityQueue {
         if (this.items.length === 0) return null;
 
         let targetIndx = 0;
-        for (let i = 0; i < this.items.length; i++){
+        for (let i = 1; i < this.items.length; i++){
             if (type === 'highest' && this.items[i].priority > this.items[targetIndx].priority) targetIndx = i;
             if (type === 'lowest' && this.items[i].priority < this.items[targetIndx].priority) targetIndx = i;
-            if (type === 'oldest' && this.items[i].order > this.items[targetIndx].order) targetIndx = i;
-            if (type === 'newest' && this.items[i].order < this.items[targetIndx].order) targetIndx = i;
+            if (type === 'oldest' && this.items[i].order < this.items[targetIndx].order) targetIndx = i;
+            if (type === 'newest' && this.items[i].order > this.items[targetIndx].order) targetIndx = i;
         }
-        const removed = splice(targetIndx, 1) [0];
+        const removed = this.items.splice(targetIndx, 1) [0];
         return removed.item;
     }
 }
