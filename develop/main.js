197807bce149f3f083479c7c31175bb68379819d
RANENNE <alexdriha9280@gmail.com>
2026-04-30 18:19:46 +0300

    fetch TMDB movies, add content filter, css

--- develop/main.js
+++ develop/main.js
@@ -45,3 +45,64 @@ fetch('../src/tools/data/movies.json')
 })
 
 .catch(error => console.error("Помилка завантаження фільмів:", error));
+
+
+const API_KEY = 'b37fda521ebe1ba79afa34f9da83cc65'; 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=uk-UA&region=UA&sort_by=popularity.desc&without_original_language=ru&page=1`;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const moviesContainer = document.getElementById('moviesContainer');
+async function getMovies(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        const cleanMovies = data.results.filter(movie => {
+            if (movie.original_language === 'ru') {
+                return false;
+            }
+
+            const hasRussianLetters = /[ыэъёЫЭЪЁ]/.test(movie.title);
+            if (hasRussianLetters) {
+                return false;
+            }
+            const hasRussianInOverview = /[ыэъёЫЭЪЁ]/.test(movie.overview);
+            if (hasRussianInOverview) return false;
+            
+            return true; 
+        });
+
+        console.log("Ідеально чисті фільми:", cleanMovies);
+        showMovies(cleanMovies);
+    } catch (error) {
+        console.error("Помилка:", error);
+    }
+}
+
+function showMovies(movies) {
+    moviesContainer.innerHTML = ''; 
+
+    if (movies.length === 0) {
+        moviesContainer.innerHTML = '<p style="text-align:center; width:100%;">Фільмів не знайдено.</p>';
+        return;
+    }
+
+    movies.forEach(movie => {
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('movie-card');
+
+        const posterPath = movie.poster_path ? IMG_URL + movie.poster_path : 'https://via.placeholder.com/500x750?text=Немає+постера';
+
+        movieCard.innerHTML = `
+            <img src="${posterPath}" alt="${movie.title}" style="width: 100%; border-radius: 8px; margin-bottom: 15px; object-fit: cover;">
+            <h3>${movie.title}</h3>
+            <p><strong>Рейтинг:</strong> ⭐ ${movie.vote_average.toFixed(1)} / 10</p>
+            <p><strong>Дата виходу:</strong> ${movie.release_date}</p>
+        `;
+
+        moviesContainer.appendChild(movieCard);
+    });
+}
+
+getMovies(API_URL);
