1c3dfbdb67e33891ea32d4bcbdac40a22bf51e2f
Alina <llliinaa.a08@gmail.com>
2026-05-10 22:11:48 +0300

    реструктуризація

--- develop/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const { movieGenerator } = require('./kursova');
-
-const movies = require('./movies.json');
-
-const gen = movieGenerator(movies);
-console.log(gen.next().value);
-console.log(gen.next().value);
